b17f7be0242833511e8e9772469b66d88414b3a7
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-11-24 10:39:40 +0100

    added iamges and updated app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<>
+			<div className="App">
+				. Coded by <a href="#">Your Name Here</a>. Preview Gabrielle Essence Eau
+				De Parfum A floral, solar and voluptuous interpretation composed by
+				Olivier Polge, Perfumer-Creator for the House of CHANEL. $149.99 $169.99
+				Add to Cart
+			</div>
+			<div className="attribution">
+				Challenge by{" "}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
