aec1b38ae81e12e36f6a403ee7dddf50bd12558e
Lyr01 <79974123+Lyr01@users.noreply.github.com>
2022-11-24 13:04:56 +0100

    added attribution

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,45 @@ import MobileProductImg from "./images/image-product-mobile.jpg";
 
 function App() {
 	return (
-		<>
-			<div className="App">
-				<div className="card">
-					<div className="card-img">
-						<img
-							src={DesktopProductImg}
-							alt="desktop product image"
-							className="desktop-product-img"
-						/>
-						<img
-							src={MobileProductImg}
-							alt="mobile product image"
-							className="mobile-product-img"
-						/>
-					</div>
-					<div className="card-content">
-						<small className="category">Perfume</small>
-						<h1 className="title">Gabrielle Essence Eau De Parfum</h1>
-						<p className="description">
-							A floral, solar and voluptuous interpretation composed by Olivier
-							Polge, Perfumer-Creator for the House of CHANEL.
-						</p>
-						<h1 className="price">
-							$149.99 <span className="prev-price">$169.99</span>
-						</h1>
+		<div className="App">
+			<div className="card">
+				<div className="card-img">
+					<img
+						src={DesktopProductImg}
+						alt="desktop product image"
+						className="desktop-product-img"
+					/>
+					<img
+						src={MobileProductImg}
+						alt="mobile product image"
+						className="mobile-product-img"
+					/>
+				</div>
+				<div className="card-content">
+					<small className="category">Perfume</small>
+					<h1 className="title">Gabrielle Essence Eau De Parfum</h1>
+					<p className="description">
+						A floral, solar and voluptuous interpretation composed by Olivier
+						Polge, Perfumer-Creator for the House of CHANEL.
+					</p>
+					<h1 className="price">
+						$149.99 <span className="prev-price">$169.99</span>
+					</h1>
 
-						<button className="add-to-cart">
-							<img src={CartIcon} alt="cart icon" className="cart-icon" /> Add
-							to Cart
-						</button>
-					</div>
+					<button className="add-to-cart">
+						<img src={CartIcon} alt="cart icon" className="cart-icon" /> Add to
+						Cart
+					</button>
 				</div>
 			</div>
-
-			{/* <div className="attribution">
+			<div className="attribution">
 				Challenge by
 				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
 					Frontend Mentor
 				</a>
 				. Coded by <a href="https://lyr01.github.io/">Hamza Khan</a>.
-			</div> */}
-		</>
+			</div>
+		</div>
 	);
 }
 
